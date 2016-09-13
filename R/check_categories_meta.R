e16dd1a6777ef9680c2ccc4050050661e38ee2c1
MattjCamp <mjcampbell74@gmail.com>
2016-09-13 05:44:01 -0400

    Added overall compare() check to categories meta

--- R/check_categories_meta.R
+++ R/check_categories_meta.R
@@ -2,44 +2,52 @@
 #' Check Meta Categories
 #'
 #' Compares the types of categories in datapoints
-#' @param a datapoints.compare object
+#' @param dc output from the compare_datapoints function
 #' @keywords dataset, utility, QC
 #' @export
 #' @examples
 
-check_categories_meta <- function(datapoints.compare){
+check_categories_meta <- function(dc){
 
   me <- list()
 
-  # MAKE SURE datapoints.compare IS IN THE EXPECTED FORMAT
+  # MAKE SURE dc IS IN THE EXPECTED FORMAT
 
   # CHECK CLASS
 
-  if (!"DataPoints_Compare" %in% class(datapoints.compare))
-    stop("datapoints.compare must be DataPoints_Compare object")
+  if (!"DataPoints_Compare" %in% class(dc))
+    stop("dc must be DataPoints_Compare object")
 
-  me$report <- "Check Category Metadata"
+  me$report <- "CHECK CATEGORY META"
 
-  num.cat.f <- length(names(datapoints.compare$f)) - 4
-  num.cat.q <- length(names(datapoints.compare$q)) - 4
-  names.f <- names(datapoints.compare$f)[1:num.cat.f]
-  names.q <- names(datapoints.compare$q)[1:num.cat.q]
+  test <- compare(names(dc$f), names(dc$q))
+  if (test$equal)
+    me$status <- "Category meta matches 100%"
 
-  if (num.cat.f != num.cat.q)
-    me$Num_of_Categories <- "f and q do not have the same number of categories"
+  if (!test$equal) {
 
-  cat.not.in.f <- names.q[!(names.q %in% names.f)]
-  if (length(cat.not.in.f) > 0)
-    me$Categories_Not_In_F_But_In_Q <- cat.not.in.f
+    num.cat.f <- length(names(dc$f)) - 4
+    num.cat.q <- length(names(dc$q)) - 4
+    names.f <- names(dc$f)[1:num.cat.f]
+    names.q <- names(dc$q)[1:num.cat.q]
 
-  cat.not.in.q <- names.f[!(names.f %in% names.q)]
-  if (length(cat.not.in.q) > 0)
-    me$Categories_Not_In_Q_But_In_F <- cat.not.in.q
+    if (num.cat.f != num.cat.q)
+      me$Num_of_Categories <- "f and q do not have the same number of categories"
 
-  # CHECK ORDER OF CATEGORIES IF NUMBER IS THE SAME
-  # AND THERE IS NO OVERLAP
+    cat.not.in.f <- names.q[!(names.q %in% names.f)]
+    if (length(cat.not.in.f) > 0)
+      me$Categories_Not_In_F_But_In_Q <- cat.not.in.f
 
-  # CHECK CLASS AND MODE IF THE CATEGORIES ARE THE SAME
+    cat.not.in.q <- names.f[!(names.f %in% names.q)]
+    if (length(cat.not.in.q) > 0)
+      me$Categories_Not_In_Q_But_In_F <- cat.not.in.q
+
+    # CHECK ORDER OF CATEGORIES IF NUMBER IS THE SAME
+    # AND THERE IS NO OVERLAP
+
+    # CHECK CLASS AND MODE IF THE CATEGORIES ARE THE SAME
+
+  }
 
   me
 
--- tests/testthat/test_compare_constructor.R
+++ tests/testthat/test_compare_constructor.R
@@ -26,27 +26,39 @@ q <- datapoints(q, c(1,2,5,6))
 
 # COMPARE DATAPOINTS OBJECT
 
-d <- compare_datapoints(f, q)
+dc <- compare_datapoints(f, q)
 
 test_that("compare_datapoints adds keys correctly", {
-  n <- names(d$f)
+  n <- names(dc$f)
   expect_equal(length(n), 10)
   expect_equal(n[10], "key")
   expect_equal(n[9], "key_cat_var")
   expect_equal(n[8], "key_cat")
-  expect_equal(d$f[1, 8], "-38.59175.736999Good")
-  expect_equal(d$f[1, 9], "-38.59175.736999Gooddepth")
-  expect_equal(d$f[1, 10], "-38.59175.736999Gooddepth162.0")
+  expect_equal(dc$f[1, 8], "-38.59175.736999Good")
+  expect_equal(dc$f[1, 9], "-38.59175.736999Gooddepth")
+  expect_equal(dc$f[1, 10], "-38.59175.736999Gooddepth162.0")
 })
 
 # CHECK CATEGORY META (DATA TYPES, NAMES)
 
-check.cat <- check_categories_meta(d)
+check.cat <- check_categories_meta(dc)
 
 test_that("check_categories_meta finds category metadata mismatches", {
-  expect_equal(check.cat$report, "Check Category Metadata")
+  expect_equal(check.cat$report, "CHECK CATEGORY META")
   expect_equal(check.cat$Num_of_Categories, "f and q do not have the same number of categories")
   expect_equal(check.cat$Categories_Not_In_F_But_In_Q, "New_Cat")
   expect_equal(check.cat$Categories_Not_In_Q_But_In_F, c("Sigma", "G"))
 })
 
+# CHECK IF COMPARE META CAN HANDLE THE TRUTH
+
+f <- quakes
+q <- quakes
+f <- datapoints(f, c(1,2,5))
+q <- datapoints(q, c(1,2,5))
+dc <- compare_datapoints(f, q)
+check.cat <- check_categories_meta(dc)
+
+test_that("check_categories_meta finds category metadata matches", {
+  expect_equal(check.cat$status, "Category meta matches 100%")
+})
