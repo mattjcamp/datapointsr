34cf02375f3e9711c1291a837facb15a739f778c
MattjCamp <mjcampbell74@gmail.com>
2017-03-04 07:17:42 -0500

    Simplified tests to just use wicher

--- tests/testthat/test_show_values_table_labels.R
+++ tests/testthat/test_show_values_table_labels.R
@@ -1,17 +1,23 @@
 
 context("SHOW VALUES TABLE LABELS")
 
-library(dplyr)
+library(testthat)
+library(tidyverse)
+library(wicher)
 library(datapointsr)
 
 test_that("show_values table labels", {
 
-  load("f_values.rdata")
-  load("q_values.rdata")
+  q <- wicher::wiche_graduate_projections %>%
+    filter(year == 2001,
+           grade == "g") %>%
+    long(1:7)
 
-  dp <- data_points(f_values, q_values)
+  f <- q
+
+  dp <- data_points(f, q)
   values <- show_values(dp)
 
-  expect_equal(names(values$mis_matched)[5], "f_value")
+  expect_equal(names(values$d)[10], "value.f")
 
 })
